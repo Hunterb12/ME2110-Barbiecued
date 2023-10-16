b0d2704cd239ddb28fa0ddb3d26dd80228165f9b
Hunterb12 <hunterbennett1212@gmail.com>
2023-10-15 20:29:18 -0400

    Update ME2110FinalProjectCode.ino
    Started Autonomous and made the drive up code. The driving back code has not been completed.

--- ME2110FinalProjectCode/ME2110FinalProjectCode.ino
+++ ME2110FinalProjectCode/ME2110FinalProjectCode.ino
@@ -12,6 +12,13 @@ int banana = 0;
 int but2 = 0; 
 int but3 = 0; 
 int but4 = 0;
+int done = 0;
+
+int movingMotorPin = 1;
+int liftMotorPin = 2;
+int solenoidPin = 1;
+int clawPneumaticPin = 2;
+
 
 //Create Encoder Handler
 void encHandler() {
@@ -31,7 +38,35 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   banana = robot.readButton(1);
-  if(banana == 1){
+  if(banana == 1 && done == 0){
     //Run Code
+    initTime = millis();
+    
+    //Move the robot forward until it hits the center
+    while(but2 != 1 || millis() <= initTime + 3000 || banana == 1){
+      robot.moveMotor(movingMotorPin, 1, 255);
+      //Open at 1 second
+      if(millis() >= initTime + 1000){
+        robot.digital(solenoidPin, 1);
+      }
+
+      //Open claw at 2 seconds
+      if(millis() >= initTime + 2000){
+        robot.digital(clawPneumaticPin,1);
+      }
+    }
+    robot.moveMotor(movingMotorPin,1,0);
+    robot.digital(solenoidPin,0)
+    delay(10000);
+
+    //Lift nuke
+
+    //Drive backwards using IR sensor and button as fail safe
+
+
+
+
+
+    done = 1;
   }
 }
