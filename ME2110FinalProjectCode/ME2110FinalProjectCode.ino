4e47e4841f3af78d5a2d6cc7a27bd7af08160b27
Hunterb12 <hunterbennett1212@gmail.com>
2023-11-02 00:10:29 -0400

    Update ME2110FinalProjectCode.ino

--- ME2110FinalProjectCode/ME2110FinalProjectCode.ino
+++ ME2110FinalProjectCode/ME2110FinalProjectCode.ino
@@ -8,6 +8,7 @@ unsigned long m1spd = 0;
 unsigned long m2spd = 0;
 unsigned long initTime = 0;
 unsigned long stopTime = 0;
+unsigned long currentTime = 0;
 int banana = 0; 
 int but2 = 0; 
 int but3 = 0; 
@@ -18,6 +19,7 @@ int movingMotorPin = 1;
 int liftMotorPin = 2;
 int solenoidPin = 1;
 int clawPneumaticPin = 2;
+int liftPneumaticPin = 3;
 
 
 //Create Encoder Handler
@@ -40,33 +42,73 @@ void loop() {
   banana = robot.readButton(1);
   if(banana == 1 && done == 0){
     //Run Code
-    initTime = millis();
     
     //Move the robot forward until it hits the center
-    while(but2 != 1 || millis() <= initTime + 3000 || banana == 1){
+    currentTime = millis();
+    while(but2 != 1 && timeStamp() <= 10000 && banana == 1){
       robot.moveMotor(movingMotorPin, 1, 255);
       //Open at 1 second
-      if(millis() >= initTime + 1000){
+      if(timeStamp() >= 2000){
         robot.digital(solenoidPin, 1);
       }
 
       //Open claw at 2 seconds
-      if(millis() >= initTime + 2000){
+      if(timeStamp() >= 3000){
         robot.digital(clawPneumaticPin,1);
       }
+      currentTime = millis();
     }
     robot.moveMotor(movingMotorPin,1,0);
     robot.digital(solenoidPin,0)
-    delay(10000);
+    robot.digital(clawPneumaticPin,0);
 
-    //Lift nuke
+    //Move slightly Back to avoid hitting Calutron
+    if(banana == 1){
+      robot.moveMotor(movingMotorPin,2,100);
+      delay(200);
+      robot.moveMotor(movingMotorPin,2,0);
+    }
 
-    //Drive backwards using IR sensor and button as fail safe
+    //Lift nuke and Sit until 25 second mark
+    currentTime = millis();
+    while(timeStamp() <= 25000 && banana == 1){
+      //raise for 6 seconds
+      if(timeStamp() <= 16000){
+        robot.moveMotor(liftMotorPin, 1, 255);
+        robot.digital(liftPneumaticPin, 1);
+      }
+      else{
+        robot.moveMotor(liftMotorPin, 1, 0);
+      }
+    }
+    robot.digital(liftPneumaticPin, 0);
 
+    //Drive backwards to Barbie Land and lower Scissor Lift
+    while(timeStamp() <= 38000 && banana == 1){
+      robot.moveMotor(movingMotorPin,2,255);
+      robot.moveMotor(liftMotorPin, 2, 100);
+    }
+    robot.moveMotor(movingMotorPin,2,0);
+    robot.moveMotor(liftMotorPin, 2, 100);
 
 
 
 
     done = 1;
+    shutDown();
+    delay(1000000);
   }
+}
+
+unsigned long timeStamp(){
+  return millis() - initTime;
+}
+
+void shutDown(){
+  robot.moveMotor(1,1,0);
+  robot.moveMotor(2,1,0);
+  robot.digital(1, 0);
+  robot.digital(2, 0);
+  robot.digital(3, 0);
+  robot.digital(4, 0);
 }
